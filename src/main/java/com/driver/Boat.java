2a877e215c8f6f24aae2db88c21d1038ace8da9b
unknown <hemant38056@gmail.com>
2022-12-17 22:19:42 +0530

    Submission3

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,6 +4,11 @@ public class Boat implements WaterVehicle{
     String name;
     int capacity;
 
+    public Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     @Override
     public String getVehicleName() {
         return name;
@@ -13,4 +18,5 @@ public class Boat implements WaterVehicle{
     public int getVehicleCapacity() {
         return capacity;
     }
+
 }
